3cbf57e0f861aac15b1d1acb274b17de9093f1d6
Andrey Novikov <envek@envek.name>
2018-10-05 13:56:44 +0300

    Upgrade evil-metrics adapters for sidekiq and prometheus

--- rails_app/config/initializers/sidekiq.rb
+++ rails_app/config/initializers/sidekiq.rb
@@ -1,15 +1,3 @@
 Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Evil::Metrics::Sidekiq::ServerMiddleware
-  end
-  config.client_middleware do |chain|
-    chain.add Evil::Metrics::Sidekiq::ClientMiddleware
-  end
   Evil::Metrics::Prometheus::Exporter.start_metrics_server!
-end
-
-Sidekiq.configure_client do |config|
-  config.client_middleware do |chain|
-    chain.add Evil::Metrics::Sidekiq::ClientMiddleware
-  end
 end
